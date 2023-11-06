1f08e2a2b53992433d884fe13d1ffe483156c6b0
BrnGomes0 <brunownog05@gmail.com>
2023-11-06 16:00:20 -0300

    feat: creating a file _api.py

--- core/configs.py
+++ core/configs.py
@@ -1,13 +1,13 @@
-from pydantic.v1 import BaseSetting
+from pydantic.v1 import BaseSettings
 from sqlalchemy.orm import declarative_base
 
-class Settings(BaseSetting):
+class Settings(BaseSettings):
     """
         Configurações gerais utilizadas em nossa aplicação!
     """
 
     API_V1_STR: str = 'api/v1'
-    DB_URL: str = 'mysql+asyncmy://root@127.0.0.1:3306/etscurso'
+    DB_URL: str = 'mysql+asyncmy://root@127.0.0.1:3306/etscursos'
     DBBaseModel = declarative_base()
 
     class Config:
